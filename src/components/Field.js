1727a5a79a9f137544fec92fc30fd18a991a28af
lizuwka <mur.mur.may.li@gmail.com>
2023-04-08 21:31:14 +0300

    sign-in

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 
-const Field = ({className, props}) => {
+const Field = ({className, ...props}) => {
   const classes = classNames('border border-green-700 rounded m-1', className)
 
   return <input className={classes} {...props} />;
