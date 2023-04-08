306f75bc41aed6f0770d4d7e312d1a57c65a0a03
Hlib Hryshko <glebg27@gmail.com>
2023-04-08 19:53:32 +0200

    scroll is 50% implemented

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,31 +1,32 @@
 import Calendar from "../components/Calendar";
 import CategoriesSidebar from "../components/CategoriesSideBar";
-import { GoThreeBars } from "react-icons/go"
+import { GoThreeBars } from "react-icons/go";
 import { useState } from "react";
 
 const MainPage = () => {
-  const [isExpanded, setIsExpanded] = useState(true)
+  const [isExpanded, setIsExpanded] = useState(true);
 
-  // const scrollContainer = document.scrollingElement;
+  const scrollContainer = document.scrollingElement;
 
-  // scrollContainer.addEventListener("wheel", (evt) => {
-  //     evt.preventDefault();
-  //     scrollContainer.scrollLeft += evt.deltaY;
-  // });
-  
+  scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+  });
+
   return (
     <div className="flex flex-row">
-      <div className="flex">{
-        (isExpanded && <CategoriesSidebar />)
-      }
-      <GoThreeBars size={30} onClick={() => {
-        setIsExpanded(!isExpanded)
-      }}/>
+      <div className="flex">
+        {isExpanded && <CategoriesSidebar />}
+        <GoThreeBars
+          size={30}
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+        />
       </div>
-      
-      <div id="calendar" style={{overflowX:"scroll"}} onScroll={()=>{
-        
-      }}><Calendar /></div>
+      <div id="calendar" className="overflow-auto" onScroll={() => {}}>
+        <Calendar />
+      </div>
     </div>
   );
 };
