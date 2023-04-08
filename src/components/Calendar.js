183abfa9f4cf8dd6c3dee1852fa9fddae587d0a2
Hlib Hryshko <glebg27@gmail.com>
2023-04-08 14:15:05 +0200

    7 hard coded days as a calendar component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,15 @@
+import CalendarTile from "./CalendarTile";
+
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return <div>
+    <CalendarTile />
+    <CalendarTile />
+    <CalendarTile />
+    <CalendarTile />
+    <CalendarTile />
+    <CalendarTile />
+    <CalendarTile />
+  </div>;
 };
 
 export default Calendar;
