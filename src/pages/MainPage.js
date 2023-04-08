de9f3beec17f5081dc724a0abc6b963766ffaf32
DonnieMix <kirik44derkach@gmail.com>
2023-04-08 20:36:08 +0300

    start of sidebar design

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,31 @@
 import Calendar from "../components/Calendar";
 import CategoriesSidebar from "../components/CategoriesSideBar";
+import { GoThreeBars } from "react-icons/go"
+import { useState } from "react";
 
 const MainPage = () => {
+  const [isExpanded, setIsExpanded] = useState(true)
+
+  // const scrollContainer = document.scrollingElement;
+
+  // scrollContainer.addEventListener("wheel", (evt) => {
+  //     evt.preventDefault();
+  //     scrollContainer.scrollLeft += evt.deltaY;
+  // });
+  
   return (
-    <div>
-      <CategoriesSidebar />
-      <Calendar />
+    <div className="flex flex-row">
+      <div className="flex">{
+        (isExpanded && <CategoriesSidebar />)
+      }
+      <GoThreeBars size={30} onClick={() => {
+        setIsExpanded(!isExpanded)
+      }}/>
+      </div>
+      
+      <div id="calendar" style={{overflowX:"scroll"}} onScroll={()=>{
+        
+      }}><Calendar /></div>
     </div>
   );
 };
