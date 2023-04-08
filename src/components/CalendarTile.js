ae3c61c059882b169252a429217cb466db8014dd
DonnieMix <kirik44derkach@gmail.com>
2023-04-08 18:28:02 +0300

    named CalendarTile elems

--- src/components/CalendarTile.js
+++ src/components/CalendarTile.js
@@ -1,9 +1,9 @@
 const CalendarTile = ({day, timetable}) => {
 
-  return <div className="shadow-tile box-border w-96 p-6 border-2 border-secondary-green rounded-[14px]">
-      <div className="font-bold text-xl uppercase">{day} <br /></div>
-      <hr className="my-1 bg-primary-green" style={{border: "none", height: "2px"}}/>
-      <div className="font-medium">
+  return <div name="tile-box" className="shadow-tile box-border w-96 p-6 border-2 border-secondary-green rounded-[14px]">
+      <div name="date" className="font-bold text-xl uppercase">{day} <br /></div>
+      <hr name="date-divider-line" className="my-1 bg-primary-green" style={{border: "none", height: "2px"}}/>
+      <div name="activities" className="font-medium">
       {
         timetable.map(item => {
           return <div className="flex flex-wrap place-items-center">
@@ -16,7 +16,7 @@ const CalendarTile = ({day, timetable}) => {
             <div name="right-time-divider" className="">
               <hr className="bg-primary-green w-3px h-8 mr-2"/>
             </div>
-            <div className="underline underline-offset-1">
+            <div name="activity-name" className="underline underline-offset-1">
               {item.name}
             </div>
           </div>
