3a62153f9b129cd59024983bdc30e7d9c5fd8aa9
Hlib Hryshko <glebg27@gmail.com>
2023-04-09 01:00:37 +0200

    sidebar organization navigation implementation pelicanization

--- src/components/CategoriesSideBar.js
+++ src/components/CategoriesSideBar.js
@@ -1,293 +1,133 @@
+import { useState } from "react";
 import CategoriesList from "./CategoriesList";
 import OrganizationsList from "./OrganizationsList";
 
 const CategoriesSidebar = () => {
+  const [currentOrganization, setCurrentOrganization] = useState({});
+  const [currentPath, setCurrentPath] = useState([]);
+
+  const handleOrganizationClick = (organization) => {
+    setCurrentOrganization(organization);
+    setCurrentPath([organization.id]);
+  };
+  const handleReturn = () => {
+    setCurrentPath(currentPath.slice(0, -1));
+  };
+  const handleCategoryClick = (category) => {
+    console.log("ADD CATEGORY: ");
+    console.log(category);
+    setCurrentPath([...currentPath, category.id]);
+  };
+
   const categories = [
     {
       name: "KMA",
-      categories: [
-        {
-          name: "CS Fac",
-          categories: [
-            {
-              name: "C#",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-            {
-              name: "Java",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-            {
-              name: "Swift",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-            {
-              name: "Prolog",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-            {
-              name: "Databases",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-            {
-              name: "Ruby",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-          ],
-          subjects: [],
-        },
-        {
-          name: "Business Fac",
-          categories: [
-            {
-              name: "Business Analytics",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-            {
-              name: "International Business",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-            {
-              name: "Marketing",
-              categories: [],
-              subjects: [
-                {
-                  name: "Year 1 Sem 1",
-                },
-                {
-                  name: "Year 1 Sem 2",
-                },
-                {
-                  name: "Year 2 Sem 1",
-                },
-                {
-                  name: "Year 2 Sem 2",
-                },
-              ],
-            },
-          ],
-          subjects: [],
-        },
-        {
-          name: "Fil Fac",
-          categories: [
-            {
-              name: "Writing Skills",
-              categories: [],
-              subjects: [
-                {
-                  name: "Group 1",
-                },
-                {
-                  name: "Group 2",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      subjects: [],
+      id: "ag2g1d2x",
     },
     {
       name: "KNU",
-      categories: [
-        {
-          name: "Organic",
-          categories: [],
-          subjects: [
-            {
-              name: "Year 1 Sem 1",
-            },
-            {
-              name: "Year 1 Sem 2",
-            },
-            {
-              name: "Year 2 Sem 1",
-            },
-            {
-              name: "Year 2 Sem 2",
-            },
-          ],
-        },
-        {
-          name: "Inorganic",
-          categories: [],
-          subjects: [
-            {
-              name: "Year 1 Sem 1",
-            },
-            {
-              name: "Year 1 Sem 2",
-            },
-            {
-              name: "Year 2 Sem 1",
-            },
-            {
-              name: "Year 2 Sem 2",
-            },
-          ],
-        },
-        {
-          name: "Analytical",
-          categories: [],
-          subjects: [
-            {
-              name: "Year 1 Sem 1",
-            },
-            {
-              name: "Year 1 Sem 2",
-            },
-            {
-              name: "Year 2 Sem 1",
-            },
-            {
-              name: "Year 2 Sem 2",
-            },
-          ],
-        },
-        {
-          name: "VMS",
-          categories: [],
-          subjects: [
-            {
-              name: "Year 1 Sem 1",
-            },
-            {
-              name: "Year 1 Sem 2",
-            },
-            {
-              name: "Year 2 Sem 1",
-            },
-            {
-              name: "Year 2 Sem 2",
-            },
-          ],
-        },
-      ],
-      subjects: [],
+      id: "23saqw2r",
+    },
+    {
+      name: "Cs Fac",
+      id: "fda123s1",
+      parentId: "ag2g1d2x",
+    },
+    {
+      name: "Buss Fac",
+      id: "af2gp4s1",
+      parentId: "ag2g1d2x",
+    },
+    {
+      name: "Fil Fac",
+      id: "jkl3uid5",
+      parentId: "ag2g1d2x",
+    },
+    {
+      name: "C#",
+      id: "lkjpfa21",
+      parentId: "fda123s1",
+    },
+    {
+      name: "Java",
+      id: "67895134",
+      parentId: "fda123s1",
+    },
+    {
+      name: "Swift",
+      id: "uyio4298",
+      parentId: "fda123s1",
+    },
+    {
+      name: "Marketing",
+      id: "9763ghdf435",
+      parentId: "af2gp4s1",
+    },
+    {
+      name: "Business Analytics",
+      id: "642535642",
+      parentId: "af2gp4s1",
+    },
+    {
+      name: "Something",
+      id: "1235134214",
+      parentId: "af2gp4s1",
+    },
+    {
+      name: "English",
+      id: "213254efda134",
+      parentId: "jkl3uid5",
+    },
+    {
+      name: "Ukrainian",
+      id: "4322t34gfsdhs",
+      parentId: "jkl3uid5",
+    },
+    {
+      name: "Organic",
+      id: "hgadfda2312145s",
+      parentId: "23saqw2r",
+    },
+    {
+      name: "Inorganic",
+      id: "gha234fsw342",
+      parentId: "23saqw2r",
+    },
+    {
+      name: "Analytical",
+      id: "jkkadf4312",
+      parentId: "23saqw2r",
+    },
+    {
+      name: "VMS",
+      id: "89052kafjasd",
+      parentId: "23saqw2r",
     },
   ];
 
-  const defOrg = "KMA";
+  // if there is no currentPath => return only organizations
+  // there is a currentPath => return children
+
   return (
     <div>
-      <OrganizationsList organizations={categories} active={categories[0]}/>
-      <hr/>
+      <OrganizationsList
+        organizations={categories.filter((category) => {
+          return !category.parentId;
+        })}
+        organization={currentOrganization}
+        onOrganizationClick={handleOrganizationClick}
+      />
+      <hr />
       <CategoriesList
-        organization={"KMA"}
-        categories={categories.find((category) => category.name === defOrg)}
+        handleReturn={handleReturn}
+        previousCategory={categories.find(
+          (category) => category.id === currentPath.at(-1)
+        )}
+        handleCategoryClick={handleCategoryClick}
+        categories={categories.filter(
+          (category) =>
+            (category.parentId && category.parentId === currentPath.at(-1))
+        )}
       />
     </div>
   );
