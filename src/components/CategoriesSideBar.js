3ccafec25ac6511d2c6886112b5196b1f533f61e
Hlib Hryshko <glebg27@gmail.com>
2023-04-09 20:12:46 +0200

    migrating data to redux store

--- src/components/CategoriesSideBar.js
+++ src/components/CategoriesSideBar.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import CategoriesList from "./CategoriesList";
 import OrganizationsList from "./OrganizationsList";
+import { useSelector } from "react-redux";
 
 const CategoriesSidebar = () => {
   const [currentOrganization, setCurrentOrganization] = useState({});
@@ -19,95 +20,10 @@ const CategoriesSidebar = () => {
     setCurrentPath([...currentPath, category.id]);
   };
 
-  const categories = [
-    {
-      name: "KMA",
-      id: "ag2g1d2x",
-    },
-    {
-      name: "KNU",
-      id: "23saqw2r",
-    },
-    {
-      name: "Cs Fac",
-      id: "fda123s1",
-      parentId: "ag2g1d2x",
-    },
-    {
-      name: "Buss Fac",
-      id: "af2gp4s1",
-      parentId: "ag2g1d2x",
-    },
-    {
-      name: "Fil Fac",
-      id: "jkl3uid5",
-      parentId: "ag2g1d2x",
-    },
-    {
-      name: "C#",
-      id: "lkjpfa21",
-      parentId: "fda123s1",
-    },
-    {
-      name: "Java",
-      id: "67895134",
-      parentId: "fda123s1",
-    },
-    {
-      name: "Swift",
-      id: "uyio4298",
-      parentId: "fda123s1",
-    },
-    {
-      name: "Marketing",
-      id: "9763ghdf435",
-      parentId: "af2gp4s1",
-    },
-    {
-      name: "Business Analytics",
-      id: "642535642",
-      parentId: "af2gp4s1",
-    },
-    {
-      name: "Something",
-      id: "1235134214",
-      parentId: "af2gp4s1",
-    },
-    {
-      name: "English",
-      id: "213254efda134",
-      parentId: "jkl3uid5",
-    },
-    {
-      name: "Ukrainian",
-      id: "4322t34gfsdhs",
-      parentId: "jkl3uid5",
-    },
-    {
-      name: "Organic",
-      id: "hgadfda2312145s",
-      parentId: "23saqw2r",
-    },
-    {
-      name: "Inorganic",
-      id: "gha234fsw342",
-      parentId: "23saqw2r",
-    },
-    {
-      name: "Analytical",
-      id: "jkkadf4312",
-      parentId: "23saqw2r",
-    },
-    {
-      name: "VMS",
-      id: "89052kafjasd",
-      parentId: "23saqw2r",
-    },
-  ];
+  const categories = useSelector((state) => state.categories);
 
   // if there is no currentPath => return only organizations
   // there is a currentPath => return children
-
 
   return (
     <div className="flex flex-row h-full bg-primary-green">
@@ -126,7 +42,7 @@ const CategoriesSidebar = () => {
         handleCategoryClick={handleCategoryClick}
         categories={categories.filter(
           (category) =>
-            (category.parentId && category.parentId === currentPath.at(-1))
+            category.parentId && category.parentId === currentPath.at(-1)
         )}
       />
     </div>
--- src/store/slices/categoriesSlice.js
+++ src/store/slices/categoriesSlice.js
@@ -2,7 +2,91 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const categoriesSlice = createSlice({
   name: "categories",
-  initialState: [],
+  initialState: [
+    {
+      name: "KMA",
+      id: "ag2g1d2x",
+    },
+    {
+      name: "KNU",
+      id: "23saqw2r",
+    },
+    {
+      name: "Cs Fac",
+      id: "fda123s1",
+      parentId: "ag2g1d2x",
+    },
+    {
+      name: "Buss Fac",
+      id: "af2gp4s1",
+      parentId: "ag2g1d2x",
+    },
+    {
+      name: "Fil Fac",
+      id: "jkl3uid5",
+      parentId: "ag2g1d2x",
+    },
+    {
+      name: "C#",
+      id: "lkjpfa21",
+      parentId: "fda123s1",
+    },
+    {
+      name: "Java",
+      id: "67895134",
+      parentId: "fda123s1",
+    },
+    {
+      name: "Swift",
+      id: "uyio4298",
+      parentId: "fda123s1",
+    },
+    {
+      name: "Marketing",
+      id: "9763ghdf435",
+      parentId: "af2gp4s1",
+    },
+    {
+      name: "Business Analytics",
+      id: "642535642",
+      parentId: "af2gp4s1",
+    },
+    {
+      name: "Something",
+      id: "1235134214",
+      parentId: "af2gp4s1",
+    },
+    {
+      name: "English",
+      id: "213254efda134",
+      parentId: "jkl3uid5",
+    },
+    {
+      name: "Ukrainian",
+      id: "4322t34gfsdhs",
+      parentId: "jkl3uid5",
+    },
+    {
+      name: "Organic",
+      id: "hgadfda2312145s",
+      parentId: "23saqw2r",
+    },
+    {
+      name: "Inorganic",
+      id: "gha234fsw342",
+      parentId: "23saqw2r",
+    },
+    {
+      name: "Analytical",
+      id: "jkkadf4312",
+      parentId: "23saqw2r",
+    },
+    {
+      name: "VMS",
+      id: "89052kafjasd",
+      parentId: "23saqw2r",
+    },
+  ],
   reducers: {
     addCategories(state, action) {
       return [...state, ...action.payload];
