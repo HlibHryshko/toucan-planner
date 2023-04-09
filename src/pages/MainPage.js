12f79e6fc778aa860d72805a4afdeab8b78c99c2
Hlib Hryshko <glebg27@gmail.com>
2023-04-09 19:53:47 +0200

    displaying the starting page, when there is no organizations

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,8 @@ import Calendar from "../components/Calendar";
 import CategoriesSidebar from "../components/CategoriesSideBar";
 import { GoThreeBars } from "react-icons/go";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import StartingPage from "./StartingPage";
 
 const MainPage = () => {
   const [isExpanded, setIsExpanded] = useState(true);
@@ -14,19 +16,25 @@ const MainPage = () => {
   });
 
   return (
-    <div className="flex flex-row bg-background-green">
-      <div className="flex">
-        {isExpanded && <CategoriesSidebar />}
-        <GoThreeBars
-          size={30}
-          onClick={() => {
-            setIsExpanded(!isExpanded);
-          }}
-        />
-      </div>
-      <div id="calendar" className="overflow-auto" onScroll={() => {}}>
-        <Calendar />
-      </div>
+    <div>
+      {useSelector((state) => state.categories).length === 0 ? (
+        <StartingPage />
+      ) : (
+        <div className="flex flex-row bg-background-green">
+          <div className="flex">
+            {isExpanded && <CategoriesSidebar />}
+            <GoThreeBars
+              size={30}
+              onClick={() => {
+                setIsExpanded(!isExpanded);
+              }}
+            />
+          </div>
+          <div id="calendar" className="overflow-auto" onScroll={() => {}}>
+            <Calendar />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
