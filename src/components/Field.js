fb986a8886f84b8c884e277e2e3765954b4a07b0
Hlib Hryshko <glebg27@gmail.com>
2023-04-09 12:02:28 +0200

    added visibility change in login/signup forms

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,12 +1,40 @@
 import classNames from "classnames";
+import VisibilityChanger from "./VisibilityChanger";
+import { useState } from "react";
 
 const Field = ({ password, className, ...props }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowPassword(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowPassword(false);
+  };
+
   const classes = classNames(
     "border border-green-700 rounded-xl m-1",
     className
   );
 
-  return <input type={password && 'password' }className={classes} {...props} />;
+  return (
+    <div className="relative flex">
+      <input
+        type={password && !showPassword ? "password" : ""}
+        className={classes}
+        {...props}
+      />
+      {password && (
+        <div className="absolute top-9 right-12">
+          <VisibilityChanger
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Field;
